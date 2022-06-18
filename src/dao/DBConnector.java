01fbd62ccc62d00fa34f313ec2f57672d01a6615
jtrowb4 <jtrowb4@wgu.edu>
2022-06-18 14:48:05 -0500

    testing and db export

--- src/dao/DBConnector.java
+++ src/dao/DBConnector.java
@@ -13,7 +13,7 @@ public abstract class DBConnector {
 
     private static final String protocol = "jdbc";
     private static final String vendor = ":mysql:";
-    private static final String location = "//localhost/";
+    private static final String location = "//127.0.0.1/";
     private static final String databaseName = "client_schedule";
     private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = SERVER"; // LOCAL
     private static final String driver = "com.mysql.cj.jdbc.Driver"; // Driver reference
